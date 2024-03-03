df6768e82a9a347fb61b058023cf1ec29c86d5ec
Rija Rizki <52735927+bangze-93@users.noreply.github.com>
2024-03-03 10:55:36 +0700

    Update mon-slave.sh

--- mon-slave.sh
+++ mon-slave.sh
@@ -4,8 +4,8 @@ chat_id="-79177xxxx" change to yours
 url=https://api.telegram.org/bot$token/sendMessage
 msg_title="SLAVE IS NOT RUNNING...!"
 spr="======================"
-ctr=`mysql -u user -pSeCr3t -e 'show slave status\G' | grep -wP "Slave_IO_Running|Slave_SQL_Running" | grep Yes | wc -l`
-msg_content=`mysql -u user -pSeCr3t -e 'show slave status\G' | grep -wP "Slave_IO_Running|Slave_SQL_Running|Last_SQL_Error|Last_IO_Error" | awk '{ sub(/^[ \t]+/, ""); print }'`
+ctr=$(mysql -u user -pSeCr3t -e 'show slave status\G' | grep -wP "Slave_IO_Running|Slave_SQL_Running" | grep Yes | wc -l)
+msg_content=$(mysql -u user -pSeCr3t -e 'show slave status\G' | grep -wP "Slave_IO_Running|Slave_SQL_Running|Last_SQL_Error|Last_IO_Error" | awk '{ sub(/^[ \t]+/, ""); print }')
 if [[ $ctr -lt 2 ]]; then
    curl -sX POST $url -d chat_id=$chat_id -d text="$msg_title%0A$spr%0A$msg_content"
 fi
